72435d838460fc8162b5839b7832f6a82895fe9f
JuanCalderoM <juandcalderoncop@gmail.com>
2024-09-10 21:58:42 -0500

    cambio de test para verificar bluprint route

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 
 # app.py
 from app import create_app
-
+import unittest
 app = create_app()
+
+@app.cli.command()
+def test():
+    tests = unittest.TestLoader().discover('test')
+    unittest.TextTestRunner().run(tests)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test/test_base.py
+++ test/test_base.py
@@ -1,33 +1,30 @@
 from flask_testing import TestCase
 from flask import current_app, url_for
-from main import main as app
+from main import create_app  # Asegúrate de importar tu función de creación de app
+
 class MainTest(TestCase):
     def create_app(self):
+        app = create_app()
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
         return app
-    #Aqui verifico la existencia del app
+
     def test_app_exists(self):
         self.assertIsNotNone(current_app)
-    #Aqui verifico el estado del app
+
     def test_app_in_test_mode(self):
         self.assertTrue(current_app.config['TESTING'])
-    #aqui verifico la redirecion de un metodo a oto
-    def test_inicio_redirect(self):
-        response=self.client.get(url_for('index'))
-        self.assertRedirects(response,url_for('blog'))
-    #aqui verifico si se  logro un get
+
     def test_coment_get(self):
-        response=self.client.get(url_for('contactanos_view'))
+        response = self.client.get('/contactanos')
         self.assert200(response)
-    #aqui se aprobueba un post
+
     def test_commet_post(self):
         fake_form = {
-            'Nombre': 'fake',
-            'Correo Electronico': 'fake@fake.com',
-            'Tema':'prueba test',
-            'commentario':'lorem moment fake'
-
+            'name': 'fake',
+            'email': 'fake@fake.com',
+            'subject': 'prueba test',
+            'comments': 'lorem moment fake'
         }
-        response = self.client.post(url_for('contactanos_view'), data=fake_form)
+        response = self.client.post('/contactanos', data=fake_form, follow_redirects=True)
         self.assert200(response)
