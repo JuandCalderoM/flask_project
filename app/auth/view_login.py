a56cbb2f2f34aba5a8cf979a2b71581db5ee7746
JuanCalderoM <juandcalderoncop@gmail.com>
2024-09-17 22:13:50 -0500

    commit que arregla distintos problemas de los template y tambien ya agrega lo que es como agregar un usuario pero con la contraseña ya encriptada y con la desencrptacion para el logeo

--- app/auth/view_login.py
+++ app/auth/view_login.py
@@ -1,7 +1,9 @@
 from . import auth
 from flask import render_template, redirect, url_for, session, flash
-from app.forms import LoginForm
+from app.forms import LoginForm,Signup,Publicacion
 from app.models import UserModel
+from app.firebase_service import  get_user_exit ,put_user
+from werkzeug.security import generate_password_hash, check_password_hash
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     login_form = LoginForm()
@@ -11,13 +13,18 @@ def login():
     if login_form.validate_on_submit():
         username = login_form.username.data
         password = login_form.password.data
+
+        # Obtener los datos del usuario (incluyendo la contraseña encriptada) desde la base de datos
+        user = get_user_exit(username)
         
-        user = UserModel.query(username, password)
         if user:
-            session['username'] = username
-            flash('Inicio de sesión exitoso!')
-           
-            return redirect(url_for('auth.session_page'))
+            # Verificar la contraseña ingresada con la contraseña encriptada almacenada
+            if check_password_hash(user['clave_usuario'], password):  # Aquí se asume que 'clave_usuario' es la contraseña encriptada
+                session['username'] = username
+                flash('Inicio de sesión exitoso!')
+                return redirect(url_for('auth.session_page'))
+            else:
+                flash('Nombre de usuario o contraseña incorrectos')
         else:
             flash('Nombre de usuario o contraseña incorrectos')
 
@@ -54,15 +61,15 @@ def perfil():
 @auth.route('/comentario')
 def comentario():
     username = session.get('username')
-    context={
-        'username':username
+    context = {
+        'username': username
     }
     if username:
-        return render_template('view_comment.html')
+        return render_template('view_comment.html', **context)
     else:
-        flash("necesistas de un inicio de sesion :)")
+        flash("Necesitas iniciar sesión :)")
         return redirect(url_for('auth.login'))
-
+    
 @auth.route('/usuarios')
 def usuarios():
     username = session.get('username')
@@ -74,25 +81,60 @@ def usuarios():
     else:
         flash("necesistas de un inicio de sesion :)")
         return redirect(url_for('auth.login'))
-@auth.route('/crear_user')
+@auth.route('/crear_user',methods=['GET', 'POST'])
 def crear_user():
     username = session.get('username')
+    signup=Signup()
     context={
-        'username':username
+        'username':username,
+        'signup':signup
     }
     if username:
-        return render_template('createuser.html')
+        if signup.validate_on_submit():
+            usuario_data=signup.username.data
+            password_data=signup.password.data
+            rol_usuario="admnin"
+            correo_usuario=signup.email.data
+            uaerdoc=get_user_exit(usuario_data)
+            if not uaerdoc:  # Verifica si la lista está vacía
+                password_secury=generate_password_hash(password_data)
+                put_user(password_secury,correo_usuario,usuario_data,rol_usuario)
+                flash('Usuario registrado!')
+                return redirect(url_for('auth.usuarios'))
+            else:
+                flash('este usuario ya esta registrado!')  
+                return redirect(url_for('auth.crear_user'))
+        return render_template('createuser.html',**context)
     else:
         flash("necesistas de un inicio de sesion :)")
         return redirect(url_for('auth.login'))
-@auth.route('/crear_pub')
+
+    
+    
+@auth.route('/crear_pub',methods=['GET', 'POST'])
 def crear_pub():
+    username = session.get('username')
+    publication=Publicacion()
+    context={
+        'username':username,
+        'publi':publication
+    }
+    if username:
+        if publication.validate_on_submit():
+            flash('Publicacion registrada!')  
+            return redirect(url_for('auth.ver_pub'))
+        return render_template('publicaciones.html',**context)
+    else:
+        flash("necesistas de un inicio de sesion :)")
+        return redirect(url_for('auth.login'))
+@auth.route('/ver_pub')
+def ver_pub():
     username = session.get('username')
     context={
         'username':username
     }
     if username:
-        return render_template('publicaciones.html')
+        return render_template('ver_public.html')
     else:
         flash("necesistas de un inicio de sesion :)")
         return redirect(url_for('auth.login'))
--- app/firebase_service.py
+++ app/firebase_service.py
@@ -1,7 +1,7 @@
 import os
 import firebase_admin
 from firebase_admin import credentials, firestore
-
+from werkzeug.security import  check_password_hash
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 cred_path = os.path.join(BASE_DIR, 'config', 'firebase_key.json')
 
@@ -13,24 +13,42 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 def get_user():
-    db.collection('usuario').get()
+    # Obtener referencia a la colección
+    usuarios_ref = db.collection('usuario')
+
+    # Traer todos los documentos de la colección
+    docs = usuarios_ref.stream()
+    return docs
 def get_user_by_key_and_password(usuario, clave):
     # Realizar una consulta en Firestore donde 'clave_usuario' y 'password' coincidan
     users_ref = db.collection('usuario')  # El nombre de tu colección en Firebase
-    query = users_ref.where('nombre_usuario', '==', usuario).where('clave_usario', '==', clave).stream()
+    
+    query = users_ref.where('nombre_usuario', '==', usuario).where(check_password_hash(['clave_usario'],clave)).stream()
 
     # Iterar sobre los resultados (debería haber solo uno si las claves son únicas)
     for user in query:
         user_data = user.to_dict()
         return user_data  # Devuelve los datos del usuario encontrado
     return None  # Si no se encuentra el usuario
-def get_user_rol(usuario):
+def get_user_exit(usuario):
     users_ref = db.collection('usuario')
-    query = users_ref.where('nombre_usuario', '==', usuario)
+    query = users_ref.where('nombre_usuario', '==', usuario).get()
     for user in query:
         user_data = user.to_dict()
-        return user_data.get('rol')  # Devuelve solo el rol del usuario
+        return user_data # 
     return None  # Si no se encuentra el usuario  # Devuelve el rol del usuario
+def put_user(user_clav, correo_usuario, nombre_usuario, rol_usuario):
+    # Referencia al documento del usuario
+    user_doc_ref = db.collection('usuario').document(nombre_usuario)#no se como crear u id aleaotrorio
+    
+    # Datos a actualizar o insertar en el documento
+    user_data = {
+        'clave_usuario': user_clav,
+        'correo_usuario': correo_usuario,
+        'nombre_usuario': nombre_usuario,
+        'rol_usuario': rol_usuario
+    }
+    # Usa el método 'set' para actualizar o crear el documento
+    user_doc_ref.set(user_data, merge=True)  # merge=True permite combinar los datos con los existentes
 
-    
-    
+# Ejemplo de uso
